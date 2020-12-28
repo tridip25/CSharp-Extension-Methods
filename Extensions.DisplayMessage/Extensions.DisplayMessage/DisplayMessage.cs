727fff5d97c94ae09503e4bd80344eb14998a9aa
tridip25 <74949104+tridip25@users.noreply.github.com>
2020-12-28 20:10:19 +0530

    Add files via upload

---
+++ Extensions.DisplayMessage/Extensions.DisplayMessage/DisplayMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions.DisplayMessage
+{
+    public static class DisplayMessage
+    {
+        public static string message(this String str1, String str2)
+        {
+            return $"{str1} and {str2} are bestfriends!!!";
+        }
+    }
+}
